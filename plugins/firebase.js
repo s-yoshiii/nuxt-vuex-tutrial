f4f02795d39a26460a9c974a9f15607a84b8b946
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-20 00:34:41 +0900

    Firebaseとの連携

---
+++ plugins/firebase.js
@@ -0,0 +1,8 @@
+import firebase from "firebase";
+const config = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+};
+if (!firebase.app.length) {
+  firebase.initializeApp(config);
+}
+export default firebase;
