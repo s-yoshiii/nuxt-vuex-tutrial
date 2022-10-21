2a1caf4c802c78d102c49a845d5d3fcde17051da
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-21 18:28:27 +0900

    時刻フォーマットの成形、細かい動作の修正①

--- pages/todos.vue
+++ pages/todos.vue
@@ -2,15 +2,17 @@
   <div class="form">
     <ul>
       <li v-for="todo in todos" :key="todo.id">
-        <input
-          type="checkbox"
-          v-bind:checked="todo.done"
-          @change="toggle(todo)"
-        />
-        <span v-bind:class="{ done: todo.done }"
-          >{{ todo.name }} {{ todo.create }}</span
-        >
-        <button type="button" v-on:click="remove(todo.id)">Delete</button>
+        <span v-if="todo.create">
+          <input
+            type="checkbox"
+            v-bind:checked="todo.done"
+            @change="toggle(todo)"
+          />
+          <span v-bind:class="{ done: todo.done }"
+            >{{ todo.name }} {{ todo.create.toDate() | dateFilter }}</span
+          >
+          <button type="button" v-on:click="remove(todo.id)">Delete</button>
+        </span>
       </li>
     </ul>
     <form v-on:submit.prevent="add">
@@ -21,6 +23,8 @@
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   data: function () {
     return {
@@ -48,11 +52,16 @@ export default {
       return this.$store.state.todos.todos;
     },
   },
+  filters: {
+    dateFilter: function (date) {
+      return moment(date).format("YYYY/MM/DD HH:mm:ss");
+    },
+  },
 };
 </script>
 
 <style>
-li > span.done {
+li > span span.done {
   text-decoration: line-through;
 }
 </style>
